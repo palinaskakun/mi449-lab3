b642d5ec77b933400819b756eeb9a14a9cb4e868
Palina Skakun <palinaskakun@gmail.com>
2024-02-06 00:05:12 -0500

    final

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,14 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  //LinkedIn shortcode with a logo
+  eleventyConfig.addShortcode("linkedin", function(url, altText) {
+    const logoSrc = "/images/in-logo.png"; // adjust the path based on your project structure
+    return `
+      <a href="${url}" target="_blank" rel="noopener noreferrer">
+        <img src="${logoSrc}" alt="${altText}" />
+      </a>`;
+  });
+
 };
